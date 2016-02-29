ebf5ab6c253609de24109c78b07b77b9fe9526a4
nbwns <nicolas.bauwens@gmail.com>
2016-02-29 20:49:36 +0100

    first commit

---
+++ design/js/functions.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {    
+    $("a[rel='image1']").colorbox();
+    $("a[rel='image2']").colorbox();
+    //$.fn.HoverImageText.defaults.AnimShow = { marginTop: "241px", height: "show" };
+    //$.fn.HoverImageText.defaults.AnimHide = { marginTop: "250px", height: "hide" }; 
+    $("#coverImage").showFeatureText();
+
+    $("#moreLink").click(function (event) {
+        event.preventDefault();
+        $("#moreDiv").slideToggle("slow");
+        $("html").animate({ scrollTop: $(document).height() }, "slow");
+        return false;
+    });
+    $("#moreLink a").click(function (event) {
+        event.preventDefault();
+        $("#moreDiv").slideUp();
+    });
+});
+
+$.fn.showFeatureText = function () {
+    return this.each(function () {
+        var box = $(this);
+        var text = $('p', this);
+
+        text.hide();
+
+        //box.hover(function () {
+        //    text.show("fast");
+        //}, function () {
+        //    text.hide("slow");
+        //});
+    });
+}
+
+
